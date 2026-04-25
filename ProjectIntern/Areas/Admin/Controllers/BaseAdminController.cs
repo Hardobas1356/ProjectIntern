25c4ead7067213c34df00840455448c80ec8a136
Hardobas <kalinhabacha@gmail.com>
2026-04-25 12:02:25 +0300

    Made user controller for admin

---
+++ ProjectIntern/Areas/Admin/Controllers/BaseAdminController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace ProjectIntern.Areas.Admin.Controllers;
+
+//[Authorize(Roles = "Admin")]
+[Area("Admin")]
+public abstract class BaseAdminController : Controller
+{
+    public bool IsUserAuthenticated()
+    {
+        return this.User.Identity?.IsAuthenticated ?? false;
+    }
+
+    public Guid? GetUserId()
+    {
+        Guid? userId = null;
+
+        bool IsAuthenticated = this.IsUserAuthenticated();
+
+        if (IsAuthenticated)
+        {
+            var identifier = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            Guid.TryParse(identifier, out Guid id);
+            userId = id;
+        }
+
+        return userId;
+    }
+}
---
+++ ProjectIntern/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectIntern.Services.Core.Interfaces;
+
+namespace ProjectIntern.Areas.Admin.Controllers;
+
+public class UserController : BaseAdminController
+{
+    private IApplicationUserService applicationUserService;
+    private ILogger<UserController> logger;
+
+    public UserController(IApplicationUserService applicationUserService, ILogger<UserController> logger)
+    {
+        this.applicationUserService = applicationUserService;
+        this.logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public async Task<IActionResult> AllUsers(string? searchTerm, int pageNumber = 1)
+    {
+        try
+        {
+            int pageSize = 10;
+            var result = await applicationUserService.GetAllUsersAdminAsync(pageNumber, pageSize, searchTerm);
+
+            ViewData["SearchTerm"] = searchTerm;
+            return View(result);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error occurred while fetching users in Admin Area.");
+            TempData["ErrorMessage"] = "Could not load users at this time.";
+            return RedirectToAction("Index", "Home", new { area = "" });
+        }
+    }
+}
