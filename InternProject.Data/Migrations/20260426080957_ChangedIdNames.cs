35a28bb97b1ce5d173abb92179de3262ef158544
Hardobas <kalinhabacha@gmail.com>
2026-04-26 11:25:59 +0300

    Added migrations

---
+++ InternProject.Data/Migrations/20260426080957_ChangedIdNames.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectIntern.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedIdNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "WorkDayAssigmentId",
+                table: "WorkDayAssignments",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "TopicID",
+                table: "Topics",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "InternshipSpecialityID",
+                table: "InternshipSpecialities",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "WorkDayAssignments",
+                newName: "WorkDayAssigmentId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Topics",
+                newName: "TopicID");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "InternshipSpecialities",
+                newName: "InternshipSpecialityID");
+        }
+    }
+}
