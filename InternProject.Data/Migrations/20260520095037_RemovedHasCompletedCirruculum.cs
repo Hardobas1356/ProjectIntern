896ac3ebd7a2fb0119fec0c47b52c1163ed06c51
Hardobas <kalinhabacha@gmail.com>
2026-05-20 12:52:01 +0300

    Added migration

---
+++ InternProject.Data/Migrations/20260520095037_RemovedHasCompletedCirruculum.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectIntern.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedHasCompletedCirruculum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasCompletedCurriculum",
+                table: "AspNetUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasCompletedCurriculum",
+                table: "AspNetUsers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
